5a1ddc89ee1a8cc3ae47bb570aaee876fb4cdcda
OmarJam456 <omar@hosein.com>
2024-01-04 21:14:47 -0500

    we so back

--- js/classes.js
+++ js/classes.js
@@ -1,124 +1,75 @@
 class Sprite {
-    constructor({
-      position,
-      imageSrc,
-      scale = 1,
-      framesMax = 1,
-      offset = { x: 0, y: 0 }
-    }) {
+    constructor({position,imageSrc }) {
       this.position = position
       this.width = 50
       this.height = 150
       this.image = new Image()
       this.image.src = imageSrc
-      this.scale = scale
-      this.framesMax = framesMax
-      this.framesCurrent = 0
-      this.framesElapsed = 0
-      this.framesHold = 5
-      this.offset = offset
     }
-  
+    
     draw() {
-      c.drawImage(
-        this.image,
-        this.framesCurrent * (this.image.width / this.framesMax),
-        0,
-        this.image.width / this.framesMax,
-        this.image.height,
-        this.position.x - this.offset.x,
-        this.position.y - this.offset.y,
-        (this.image.width / this.framesMax) * this.scale,
-        this.image.height * this.scale
-      )
+      c.drawImage(this.image, this.position.x, this.position.y)   
     }
-  
-    animateFrames() {
-      this.framesElapsed++
-  
-      if (this.framesElapsed % this.framesHold === 0) {
-        if (this.framesCurrent < this.framesMax - 1) {
-          this.framesCurrent++
-        } else {
-          this.framesCurrent = 0
-        }
-      }
-    }
-  
+    
     update() {
       this.draw()
-      this.animateFrames()
     }
   }
   
-  class Fighter extends Sprite {
-    constructor({
-      position,
-      velocity,
-      color = 'red',
-      imageSrc,
-      scale = 1,
-      framesMax = 1,
-      offset = { x: 0, y: 0 },
-      sprites,
-      attackBox = { offset: {}, width: undefined, height: undefined }
-    }) {
-      super({
-        position,
-        imageSrc,
-        scale,
-        framesMax,
-        offset
-      })
-  
+  class Fighter {
+    constructor({position, velocity, color = 'red', offset}) {
+      this.position = position
       this.velocity = velocity
       this.width = 50
       this.height = 150
       this.lastKey
-      this.attackBox = {
+      this.hitBox = {
         position: {
           x: this.position.x,
           y: this.position.y
         },
-        offset: attackBox.offset,
-        width: attackBox.width,
-        height: attackBox.height
+        offset,
+        width:100,
+        height: 50
       }
       this.color = color
       this.isAttacking
       this.health = 100
-      this.framesCurrent = 0
-      this.framesElapsed = 0
-      this.framesHold = 5
-      this.sprites = sprites
-      this.dead = false
-  
-      for (const sprite in this.sprites) {
-        sprites[sprite].image = new Image()
-        sprites[sprite].image.src = sprites[sprite].imageSrc
+    }
+
+    draw() {
+      c.fillstyle = this.color
+      c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      
+      //hit box
+      if (this.isAttacking) {
+        c.fillStyle = 'green' 
+        c.fillRect (
+          this.hitBox.position.x,
+          this.hitBox.position.y,
+          this.hitBox.width,
+          this.hitBox.height
+        )
       }
     }
-
     
     update() {
-        this.draw(); // Trigger the drawing process
-        this.hitBox.position.x = this.position.x + this.hitBox.offset.x;
-        this.hitBox.position.y = this.position.y;
+        this.draw() // Trigger the drawing process
+        this.hitBox.position.x = this.position.x + this.hitBox.offset.x
+        this.hitBox.position.y = this.position.y
         
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y; // Update the position 
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y // Update the position 
 
-        if (this.position.y + this.height + this.velocity.y >= canvas.height-72) {
-            this.velocity.y = 0;
-        } else {
-            this.velocity.y += gravity; // Stopping gravity at the bottom of the screen
-        }
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity; // Stopping gravity at the bottom of the screen
     }
     
-    attack() {
+   attack() {
         this.isAttacking = true;
         setTimeout(() => {
             this.isAttacking = false;
         }, 100);
-    }
+   }
 }
